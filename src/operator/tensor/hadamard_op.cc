b3e230cbb1efbcd71085c07ff0b693fe928ffff2
Chen <chenbeid@186590ccd8b7.ant.amazon.com>
2017-03-20 15:46:21 -0700

    finally get matrix output

--- src/operator/tensor/hadamard_op.cc
+++ src/operator/tensor/hadamard_op.cc
@@ -1,6 +1,6 @@
 #include "./hadamard_op.h"
-#include "./elemwise_binary_op.h"
-#include "./elemwise_binary_broadcast_op.h"
+//#include "./elemwise_binary_op.h"
+//#include "./elemwise_binary_broadcast_op.h"
 
 namespace mxnet {
 namespace op {
@@ -9,8 +9,8 @@ MXNET_OPERATOR_REGISTER_HADAMARD(dense_inplace)
 .set_attr<nnvm::FGradient>("FGradient", ElemwiseGradUseIn{"_backward_dense_inplace"});
 
 NNVM_REGISTER_OP(_backward_dense_inplace)
-.set_num_inputs(3)
-.set_num_outputs(1)
+.set_num_inputs(1)
+.set_num_outputs(3)
 .set_attr<nnvm::TIsBackward>("TIsBackward", true)
 .set_attr<FResourceRequest>("FResourceRequest",
 [](const NodeAttrs& attrs) {
